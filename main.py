bd8073cddf1937bd3180a0e5a752e41f13696656
wooong1223 <jiwoong7008@gmail.com>
2024-04-19 14:55:53 +0900

    ADD new features

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def check_available():
     return 'It works!'
 
+def split_odd_even(nums=[1,2,3,4]):
+    odds = []
+    evens = []
+    for i in nums:
+        if i%2==0:
+            evens.append(i)
+        else:
+            odds.append(i)
+    return (odds,evens)
+
 if __name__ == '__main__':
-    print(check_available())
+    print(split_odd_even())
